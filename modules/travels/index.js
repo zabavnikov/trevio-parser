42f4bb3d4f19c173911c78b92b50ee3874174ca9
Denis <zabavnikov@trevio.ru>
2020-02-26 17:01:05 +0700

    Доработка парсера путешествий.

--- modules/travels/index.js
+++ modules/travels/index.js
@@ -1,4 +1,4 @@
-const sqlMigrator = require('../../utils/sql-migrator');
+const toSql = require('../../utils/toSql');
 const Travel = require('./models/Travel');
 
 let limit = 300, offset = 0;
@@ -11,7 +11,20 @@ function run() {
       limit,
     })
     .then(travels => {
-      sqlMigrator(travels, 'travels');
+      travels.forEach(travel => {
+        toSql({
+          id: travel.id,
+          owner_id: travel.owner_id,
+          title: travel.title,
+          text: travel.text,
+          budget: travel.budget,
+          date_start: travel.date_start,
+          date_end: travel.date_end,
+          created_at: travel.created_at,
+          updated_at: travel.updated_at,
+          published_at: travel.published_at,
+        }, 'travels');
+      });
 
       offset += limit;
 
